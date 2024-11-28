ebfcaffc34b1595a9e2f769f23a76140abf76225
tarde <nilarroyo43@gmail.com>
2024-11-28 18:57:47 +0100

    Index Screen done

--- app/src/main/java/com/example/hospitalapp/MainActivity.kt
+++ app/src/main/java/com/example/hospitalapp/MainActivity.kt
@@ -4,11 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hospitalapp.classes.CreateNurses
+import com.example.hospitalapp.classes.FirstScreen
 import com.example.hospitalapp.classes.NurseApp
 import com.example.hospitalapp.ui.theme.HospitalAppTheme
 
@@ -20,7 +38,6 @@ class MainActivity : ComponentActivity() {
             HospitalAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     LoginScreen(modifier = Modifier.run { padding(innerPadding) })
-
                 }
             }
         }
